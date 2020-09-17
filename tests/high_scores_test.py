85ac3d89e78fc697252ba3ac15351395ecf0fe1d
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-09-17 13:12:36 +0100

    added scores exist check and test passed

--- tests/high_scores_test.py
+++ tests/high_scores_test.py
@@ -7,15 +7,23 @@ from src.high_scores import latest, personal_best, personal_top_three
 
 class HighScoresTest(unittest.TestCase):
 
-    # Tests
+    def setUp(self):
+        self.scores = [34, 54, 765, 234, 1, 32, 764, 98, 901, 378, 811]
+
+        # Tests
+
+    def test_check_scores_exist(self):
+        self.assertEqual(11, len(self.scores))
 
     # Test latest score (the last thing in the list)
+
+
 
     # Test personal best (the highest score in the list)
 
     # Test top three from list of scores
 
-    # Test ordered from highest tp lowest
+    # Test ordered from highest to lowest
 
     # Test top three when there is a tie
 
